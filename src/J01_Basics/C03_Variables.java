535a505d0246e9c60dc34ffebb560bdce8001b1a
sagar raj <sagarraj9170@gmail.com>
2026-08-04 08:34:00 -0700

    Type of variable

--- src/J01_Basics/C03_Variables.java
+++ src/J01_Basics/C03_Variables.java
@@ -5,5 +5,10 @@ public class C03_Variables {
        int a=10;
        int b=5;
        System.out.println("a");
+       System.out.println(a);
+       String name = "Sagar raj";
+       System.out.println(name);
+       a=50;
+       System.out.println(a);
     }
 }
