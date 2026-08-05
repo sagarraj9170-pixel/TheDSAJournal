37bf6b8ebbef2d73e8dbb34a264159c7f9fe0f01
sagar raj <sagarraj9170@gmail.com>
2026-08-05 10:05:27 -0700

    data type

---
+++ src/J01_Basics/C04_Datatype.java
@@ -0,0 +1,15 @@
+package J01_Basics;
+
+public class C04_Datatype {
+    public static void main(String args[]){
+        byte b =7;
+        System.out.println(b);
+        char ch ='a';
+        System.out.println(ch);
+        boolean var = false;
+        float price=10.5;
+        int number = 45;
+        //long is used to store to large value in ineger
+        //double is strong tha large  decimal vale
+    }
+}
