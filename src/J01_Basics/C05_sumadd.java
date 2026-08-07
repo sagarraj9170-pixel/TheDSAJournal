7443a46839525892b0b0793207f297421431841c
sagar raj <sagarraj9170@gmail.com>
2026-08-07 10:31:59 -0700

    Adding the two digite number

---
+++ src/J01_Basics/C05_sumadd.java
@@ -0,0 +1,10 @@
+package J01_Basics;
+
+public class C05_sumadd {
+    public static void main(String args[]){
+        int a =10;
+        int b =5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
