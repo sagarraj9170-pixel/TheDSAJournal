3c394f29143365d6a21c3042fdbecd181ffdda43
sagar raj <sagarraj9170@gmail.com>
2026-08-08 10:46:14 -0700

    comment

--- src/J01_Basics/C05_sumadd.java
+++ src/J01_Basics/C05_sumadd.java
@@ -2,6 +2,10 @@ package J01_Basics;
 
 public class C05_sumadd {
     public static void main(String args[]){
+        //code to calculate sum
+        /*
+        multe line commit
+         */
         int a =10;
         int b =5;
         int sum = a+b;
