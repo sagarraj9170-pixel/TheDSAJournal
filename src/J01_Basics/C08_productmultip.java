1331eb7058be23be8d51930242104b62bcb1c454
sagar raj <sagarraj9170@gmail.com>
2026-08-13 22:00:50 -0700

    mult of product

---
+++ src/J01_Basics/C08_productmultip.java
@@ -0,0 +1,15 @@
+package J01_Basics;
+
+import java.util.*;
+
+public class C08_productmultip {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System .in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product= a * b;
+        System.out.println(product);
+
+    }
+
+}
