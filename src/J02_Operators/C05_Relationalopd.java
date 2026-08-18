d828b83033ad9c0dae8e5eff37182cd49a1209ee
sagar raj <sagarraj9170@gmail.com>
2026-08-18 05:51:06 -0700

    about relational opd

---
+++ src/J02_Operators/C05_Relationalopd.java
@@ -0,0 +1,18 @@
+package J02_Operators;
+import java.util.*;
+public class C05_Relationalopd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+      //  int A=10;
+        //int B=6;
+        //System.out.println((A==B));
+        //give me false answer becouse of
+        //a is not equal to equal to b?
+
+         int A=10;
+         int B=10;
+         System.out.println((A=B));
+
+
+    }
+}
