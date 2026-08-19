1e98465e0f36094460d274ccf9b94980c0f41e9e
sagar raj <sagarraj9170@gmail.com>
2026-08-19 13:09:12 -0700

    learn about Assignment opd

---
+++ src/J02_Operators/C09_Assignment.java
@@ -0,0 +1,43 @@
+package J02_Operators;
+import java.util.*;
+public class C09_Assignment {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+//        int A=10;
+//       // A=A+10;
+//        A+=10;
+//
+//        System.out.println(A);  //answer hai 20
+
+
+       // int A=10;
+       // A=A-2;
+       // A-=3;
+       // System.out.println(A);// answer is 7 hai
+
+
+     //int A=10;
+     //A=A*6;
+     //A*=6;
+     //System.out.println(A); //this ans is 60 only
+
+
+        int A=10;
+        A=A/2;
+        A/=2;
+        System.out.println(A);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
