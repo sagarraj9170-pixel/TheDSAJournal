29a0695b57ee4435127809fa0c386b6a68ff4062
sagar raj <sagarraj9170@gmail.com>
2026-08-19 12:33:07 -0700

    about logical opd

---
+++ src/J02_Operators/C08_Logicalopd.java
@@ -0,0 +1,17 @@
+package J02_Operators;
+import java.util.*;
+
+public class C08_Logicalopd {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+
+ //   System.out.println((3>2)&&(5>9));//this is logical opd
+
+    //System.out.println((3>2)||(5<0));//this is OR opd
+
+    System.out.println(!(0>5));
+
+}
+
+}
