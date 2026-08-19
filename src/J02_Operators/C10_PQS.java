fac9c966d79fdefb1ad0d0861243b2c4704626c6
sagar raj <sagarraj9170@gmail.com>
2026-08-19 13:22:02 -0700

    some new qs

---
+++ src/J02_Operators/C10_PQS.java
@@ -0,0 +1,14 @@
+package J02_Operators;
+import  java.util.*;
+
+public class C10_PQS {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x=2 ,y=5;
+
+        int exp1=(x*y/x);
+        int exp2=(x*(y/x));
+        System.out.println(exp1 +",");
+        System.out.println(exp2);
+    }
+}
