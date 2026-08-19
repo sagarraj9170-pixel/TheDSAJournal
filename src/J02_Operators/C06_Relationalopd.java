fdc0d8ec9e8333465591c2ed7094c71fdee5ee18
sagar raj <sagarraj9170@gmail.com>
2026-08-19 10:00:22 -0700

    relational or equalto

--- src/J02_Operators/C06_Relationalopd.java
+++ src/J02_Operators/C06_Relationalopd.java
@@ -4,10 +4,14 @@ import java.util.*;
 public class C06_Relationalopd {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int A= 10;
-        int B=5;
-        System.out.println((A>B));//true
+       // int A= 10;//GRATER THAN
+       // int B=5;
+       // System.out.println((A>B));//true
         //System.out.println((A<B));
+
+        int  A = 10;
+        int B =20; // less than
+        System.out.println((A<B)); // true
 
     }
 }
---
+++ src/J02_Operators/C07_relationalopd.java
@@ -0,0 +1,17 @@
+package J02_Operators;
+import java.util.*;
+
+public class C07_relationalopd {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+      //  int A=10;
+        //int B=10;//grater than isequalto
+        //System.out.println((A>=B));
+
+        //less than isequal to
+        int A=10;
+        int B=1;
+        System.out.println((A<=B));
+
+    }
+}
