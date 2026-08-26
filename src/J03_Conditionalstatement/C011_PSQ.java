10f1c53c2649697761a49dd4b51577d5b13c32bd
sagar raj <sagarraj9170@gmail.com>
2026-08-26 11:26:20 -0700

    somr psq

---
+++ src/J03_Conditionalstatement/C011_PSQ.java
@@ -0,0 +1,18 @@
+package J03_Conditionalstatement;
+import java.util.*;
+
+public class C011_PSQ {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+         int x = sc.nextInt();
+
+         if(x>0){
+             System.out.println("x is greater thn 0");
+
+         }
+         else{
+             System.out.println("x is less thn or equal 0");
+         }
+
+    }
+}
