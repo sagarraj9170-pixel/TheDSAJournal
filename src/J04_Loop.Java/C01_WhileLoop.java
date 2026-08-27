99e991a4820a83cc909f2957af9345e5cca18328
sagar raj <sagarraj9170@gmail.com>
2026-08-27 10:32:41 -0700

    whileLoop

---
+++ src/J04_Loop.Java/C01_WhileLoop.java
@@ -0,0 +1,14 @@
+package J04_Loop.Java;
+import java.util.*;
+public class C01_WhileLoop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        while(counter<100){
+            System.out.println("hello world");
+            counter++;
+
+        }
+        System.out.println("print hw 100x");
+    }
+}
