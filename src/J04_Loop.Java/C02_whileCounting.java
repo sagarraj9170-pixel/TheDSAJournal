f47789fd8045e8d32f4dfa4fb6fd9c8089d294cb
sagar raj <sagarraj9170@gmail.com>
2026-08-27 10:42:31 -0700

    some QS of whileLoop

---
+++ src/J04_Loop.Java/C02_whileCounting.java
@@ -0,0 +1,15 @@
+package J04_Loop.Java;
+import java.util.*;
+
+public class C02_whileCounting {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int counter =1;
+
+        while (counter<=100){
+            System.out.println(counter+"");
+            counter++;
+        }
+
+    }
+}
