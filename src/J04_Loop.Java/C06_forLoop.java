96a73406edbc91d992c0262e81e14800e3100167
sagar raj <sagarraj9170@gmail.com>
2026-08-31 09:16:08 -0700

    for loop

---
+++ src/J04_Loop.Java/C06_forLoop.java
@@ -0,0 +1,13 @@
+package J04_Loop.Java;
+import java.util.*;
+public class C06_forLoop {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=10;i++){
+            System.out.println("hello world");
+        }
+
+
+
+    }
+}
