6ccab7b3743adc5a3cf173cb724310229966191c
sagar raj <sagarraj9170@gmail.com>
2026-09-01 07:25:26 -0700

    doWhileLoop

---
+++ src/J04_Loop.Java/C09_doWhileLoop.java
@@ -0,0 +1,13 @@
+package J04_Loop.Java;
+import java.util.*;
+
+public class C09_doWhileLoop {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int counter=1;
+        do {
+            System.out.println("hello world");
+            counter++;
+        }while(counter<=10);
+    }
+}
