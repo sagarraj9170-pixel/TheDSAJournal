96ed17452915bfa8d02d7292bbf2ddbf74b6f6f0
sagar raj <sagarraj9170@gmail.com>
2026-09-01 07:57:59 -0700

    some PQS

---
+++ src/J04_Loop.Java/C11_PQS.java
@@ -0,0 +1,17 @@
+package J04_Loop.Java;
+import java.util.*;
+
+public class C11_PQS {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("enter your number");
+            int n =sc.nextInt();
+           if(n %  10 == 0){
+               break;
+           }
+           System.out.println(n);
+        }while(true);
+
+    }
+}
