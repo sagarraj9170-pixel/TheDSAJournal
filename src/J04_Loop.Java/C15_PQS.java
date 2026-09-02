57efe0c2533a26dde88b0b34ba8748b47b4ac5b3
sagar raj <sagarraj9170@gmail.com>
2026-09-02 12:30:52 -0700

    some pqs qustion

---
+++ src/J04_Loop.Java/C15_PQS.java
@@ -0,0 +1,12 @@
+package J04_Loop.Java;
+import java.util.*;
+
+public class C15_PQS {
+    public void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<5; i++){
+            System.out.println("hello");
+            i+=2;
+        }
+    }
+}
