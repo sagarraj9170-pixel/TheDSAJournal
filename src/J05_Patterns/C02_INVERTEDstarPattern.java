8ca911b03a2d26978c6def5f943bb2e45d175666
sagar raj <sagarraj9170@gmail.com>
2026-09-04 10:30:56 -0700

    inverted Pattern

--- src/J05_Patterns/C02_INVERTEDstarPattern.java
+++ src/J05_Patterns/C02_INVERTEDstarPattern.java
@@ -1,11 +1,15 @@
 package J05_Patterns;
 import java.util.*;
-public class C01_NestedLoopStarPattern {
-    public static void main(String[]ages){
-        Scanner sc =new Scanner(System.in);
+
+public class C02_INVERTEDstarPattern {
+    public static void main(String[] args){
+    Scanner sc =new Scanner(System .in);
+
+
+
         for(int line=1; line<=5; line++){
 
-            for(int star=1;star<=line; star++){
+            for(int star=5;star>=line; star--){
                 System.out.print("*");
 
             }
