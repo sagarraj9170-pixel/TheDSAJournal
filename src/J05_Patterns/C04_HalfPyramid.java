336a4c5e0b1c0c6e70777c2889776ac7fc6e1dd6
sagar raj <sagarraj9170@gmail.com>
2026-09-05 12:58:43 -0700

    print the num is Pyramid shap

---
+++ src/J05_Patterns/C04_HalfPyramid.java
@@ -0,0 +1,18 @@
+package J05_Patterns;
+import java.util.*;
+public class C04_HalfPyramid {
+    public static void main (String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        int n=4;
+        for (int line=1; line<=n; line++){
+            for (int number=1; number<=line; number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
---
+++ src/J05_Patterns/C05_CharacterPattern.java
@@ -0,0 +1,21 @@
+package J05_Patterns;
+import java.util.*;
+
+public class C05_CharacterPattern {
+    public static void main(String[] args){
+        Scanner  sc = new Scanner(System.in);
+
+        int n=4;
+        char ch='A';
+
+        //outer loop
+        for (int line=1; line<=n; line++){
+            //inner loop
+            for (int chars=1; chars<=line; chars++){
+                System.out.println(ch);
+
+            }
+            System.out.println();
+        }
+    }
+}
