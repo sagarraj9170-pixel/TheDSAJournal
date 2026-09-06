241063c23cceff632b00afae6a3b606569c0bfd1
sagar raj <sagarraj9170@gmail.com>
2026-09-06 09:12:33 -0700

    Syntex Code of Function

---
+++ src/J06_Function/C01_SyntexCode.java
@@ -0,0 +1,17 @@
+package J06_Function;
+
+public class C01_SyntexCode {
+
+    public static void printHelloWorld(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        return ;//return 3 mtbl; hw 3 baar print hg gaya
+    }
+    //total 5 sa 8 tak sub koi function hai
+    public static void main(String[] args){
+        printHelloWorld();
+    }
+
+}
