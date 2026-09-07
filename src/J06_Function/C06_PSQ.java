741856dd6e2a350bbe87ec8ed07f3eef8c38df36
sagar raj <sagarraj9170@gmail.com>
2026-09-07 11:18:21 -0700

    find the Binomial Cofficient

---
+++ src/J06_Function/C06_PSQ.java
@@ -0,0 +1,32 @@
+package J06_Function;
+import java.util.*;
+public class C06_PSQ {
+
+
+    public static int factorial(int n){
+        int f=1;
+        for(int i=1; i<=n; i++){
+            f=f*i;
+
+        }///////this is called function
+        return f;
+    }
+
+
+
+
+
+    public static int binCoeff(int n,int r){
+        int fact_n =factorial(n);
+        int fact_r =factorial(r);
+        int fact_nmr =factorial(n-r);
+
+        int bincoeff = fact_n /(fact_r * fact_nmr);
+        return bincoeff;
+    }
+
+
+    public static void main(String[] args){
+     System.out.println(binCoeff(5,2));
+    }
+}
