6b94ad7cb085478a87a6abd58767a61f05a3ddca
sagar raj <sagarraj9170@gmail.com>
2026-09-08 12:39:52 -0700

    function overloding

---
+++ src/J06_Function/C07_functionOverloding.java
@@ -0,0 +1,19 @@
+package J06_Function;
+import java.util.*;
+public class C07_functionOverloding {
+
+    //func to calc sum of 2 nums
+    public static int sum(int a,int b){
+        return a+b;
+    }
+
+    //func to calc sum of 3 num
+    public static int sum(int a,int b,int c){
+        return a+b+c;
+    }
+
+    public static void main(String[] args){
+System.out.println(sum(3,5));
+System.out.println(sum(5,2 ,1));
+    }
+}
