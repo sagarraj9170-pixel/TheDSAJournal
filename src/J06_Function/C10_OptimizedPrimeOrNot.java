bb24b689e34b7b6a121265e4eeccd268706642be
sagar raj <sagarraj9170@gmail.com>
2026-09-12 02:32:06 -0700

    OptimizedPrimeNumber

---
+++ src/J06_Function/C10_OptimizedPrimeOrNot.java
@@ -0,0 +1,20 @@
+package J06_Function;
+
+public class C10_OptimizedPrimeOrNot {
+
+    public static boolean isprime(int n){
+        if (n==2){
+            return true;
+        }
+        for (int i=2; i<=Math.sqrt(n); i++){
+            if (n% i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+          System.out.println(isprime(16));
+
+    }
+}
