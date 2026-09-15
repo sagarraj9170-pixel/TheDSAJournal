c480be6e3cc18000bf4ba48532eeaf0b9e00c3db
sagar raj <sagarraj9170@gmail.com>
2026-09-15 12:17:00 -0700

    find even or odd by using the function

---
+++ src/J06_Function/C14_PQS.java
@@ -0,0 +1,26 @@
+package J06_Function;
+import java.util.*;
+public class C14_PQS {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.print("Enter an integer:");
+        num =sc.nextInt();
+
+        if (isEven(num)){
+            System.out.println("Number is even");
+
+        }
+        else{
+            System.out.println("number is odd");
+        }
+    }
+    public static boolean isEven(int number){
+        if(number%2==0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
