5abaaff221c7261ced92975c982ffd100ca0d91a
sagar raj <sagarraj9170@gmail.com>
2026-09-15 12:06:32 -0700

    using function find average

---
+++ src/J06_Function/C13_PSQ.java
@@ -0,0 +1,18 @@
+package J06_Function;
+import java.util.*;
+public class C13_PSQ {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       System.out.print("input the first number:");
+       double x =sc.nextDouble();
+       System.out.print("input the second number:");
+       double y =sc.nextDouble();
+       System.out.print("input the third number");
+       double z= sc.nextDouble();
+       System.out.print("the average value is "+ average(x,y,z)+"\n");
+
+    }
+    public static double average(double x, double y ,double z){
+        return (x + y + z) / 3;
+    }
+}
