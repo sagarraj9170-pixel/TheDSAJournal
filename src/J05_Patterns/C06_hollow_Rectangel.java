177f4366bee6d552ddca8364bee41a1ec4b42c52
Sagar Raj <sagarraj9170@gmail.com>
2026-09-23 12:02:56 -0700

    patter and rec

---
+++ src/J05_Patterns/C06_hollow_Rectangel.java
@@ -0,0 +1,24 @@
+package J05_Patterns;
+import java.util.*;
+public class C06_hollow_Rectangel {
+    public static void hollow_Rectangle(int totalRows, int totalCols){
+      //outer loop
+       for (int i=1; i<=totalRows; i++){
+            //inner loop
+           for(int j=1; j<=totalCols; j++){
+               //cell (i,j)
+               if(i==1 || i==totalRows || j==1 || j==totalCols){
+                   System.out.print("*");
+               }
+               else {
+                   System.out.println("");
+               }
+           }
+
+        }
+    }
+    public static void main(String[] args){
+        hollow_Rectangle(4 ,5);
+
+    }
+}
